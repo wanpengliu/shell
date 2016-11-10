c174e27f1f547e547b8cead31de32a0b4a345998
wanpengliu <wallaceking0@hotmail.com>
2016-11-10 16:24:10 +0000

    message

--- functions/function.sh
+++ functions/function.sh
@@ -30,3 +30,60 @@ findFile() {
 findDir() {
     find $1 -maxdepth 1 -type d 2>/dev/null | wc -l
 }
+
+dirValidation() {
+  if [ -d $1 ]
+      then 
+      return 0
+      else 
+          return 1
+fi          
+}
+
+
+fileValidation() {
+    if [ -f $1 ]
+        then 
+        return 0
+        else 
+            return 1
+fi
+}
+
+printargus() {
+    echo "total number of arguments $#"
+    for x in $@ 
+        do
+            echo $x
+            done
+}
+
+
+printargus2() {
+    echo "total number of arguments $#"
+    for x in $* 
+        do
+            echo $x
+            done
+}
+
+validateargu() {
+
+  if [ "$1" -ne "$2" ]
+     then
+      echo "Expected number of input paramters are $2"
+      else
+      return 0
+ fi
+}
+
+
+testvalidateargu() {
+  
+  validateargu 2 2 
+ if [ "$?" -eq 0 ]
+     then
+      echo "success"
+ fi
+
+}
