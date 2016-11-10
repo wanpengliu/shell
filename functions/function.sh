267ce1c05784499591fad702aced8d43bd4fd6eb
wanpengliu <wallaceking0@hotmail.com>
2016-11-10 14:38:22 +0000

    message

--- functions/function.sh
+++ functions/function.sh
@@ -3,3 +3,28 @@
 greeting(){
     echo "hello $1"
 }
+
+add() {
+    echo "$1 + $2" | bc -l
+    }
+
+subtract() {
+  echo "$1 - $2" | bc -l
+}
+
+multiple() {
+    echo "$1 * $2" | bc -l
+}
+
+
+divide() {
+    echo "scale=3; $1 / $2 " | bc -l
+}
+
+findFile() {
+    find $1 -maxdepth 1 -type f 2>/dev/null |   wc -l
+}
+
+findDir() {
+    find $1 -maxdepth 1 -type d 2>/dev/null | wc -l
+}
